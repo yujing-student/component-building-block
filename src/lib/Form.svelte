402841d1d9a82a51dc0ef3e9ee92b3c8c898a9da
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-11-14 13:25:22 +0100

    fix #13 the performance is now 100% with only a h1 on the page

--- src/lib/Form.svelte
+++ src/lib/Form.svelte
@@ -3,76 +3,76 @@
     import TextButton from '$lib/Subscribe.svelte'
 </script>
 
-<form>
-    <fieldset>
-        <legend>persoonlijke gegevens</legend>
-        <label for="Voornaam" class="form-label" >Voornaam
-            <span class="star">*</span>
-            <input id="Voornaam"  type="text" required aria-required="true"
-            />
-        </label>
-        <label for="Tussenvoegsel" class="form-label" >
-            Tussenvoegsel
-            <input id="Tussenvoegsel" type="text"/>
-        </label>
-        <label for="Achternaam" class="form-label" >
-            Achternaam    <span class="star">*</span>
-            <input id="Achternaam"  type="text" required aria-required="true"/>
-        </label>
-        <label for="functie" class="form-label" >
-            Functie    <span class="star">*</span>
-            <input id="functie" type="text" required aria-required="true"/>
-        </label>
-        <label for="email" class="form-label"  >
-            E-mail    <span class="star">*</span>
-            <input id="email" type="email"/>
-        </label>
-        <label for="Mobiel" class="form-label" >
-            Mobiel    <span class="star">*</span>
-            <input id="Mobiel"  type="tel" required aria-required="true"/>
-        </label>
-        <label for="Bedrijfsnaam" class="form-label" >
-            Bedrijfsnaam    <span class="star">*</span>
-            <input id="Bedrijfsnaam"  type="text" required aria-required="true"/>
-        </label>
-        <label for="Bedrijfswebsite" class="form-label">
-            Bedrijfswebsite  <span class="star">*</span>
-            <input id="Bedrijfswebsite"    type="text" required aria-required="true" />
-        </label>
+<!--<form>-->
+<!--    <fieldset>-->
+<!--        <legend>persoonlijke gegevens</legend>-->
+<!--        <label for="Voornaam" class="form-label" >Voornaam-->
+<!--            <span class="star">*</span>-->
+<!--            <input id="Voornaam"  type="text" required aria-required="true"-->
+<!--            />-->
+<!--        </label>-->
+<!--        <label for="Tussenvoegsel" class="form-label" >-->
+<!--            Tussenvoegsel-->
+<!--            <input id="Tussenvoegsel" type="text"/>-->
+<!--        </label>-->
+<!--        <label for="Achternaam" class="form-label" >-->
+<!--            Achternaam    <span class="star">*</span>-->
+<!--            <input id="Achternaam"  type="text" required aria-required="true"/>-->
+<!--        </label>-->
+<!--        <label for="functie" class="form-label" >-->
+<!--            Functie    <span class="star">*</span>-->
+<!--            <input id="functie" type="text" required aria-required="true"/>-->
+<!--        </label>-->
+<!--        <label for="email" class="form-label"  >-->
+<!--            E-mail    <span class="star">*</span>-->
+<!--            <input id="email" type="email"/>-->
+<!--        </label>-->
+<!--        <label for="Mobiel" class="form-label" >-->
+<!--            Mobiel    <span class="star">*</span>-->
+<!--            <input id="Mobiel"  type="tel" required aria-required="true"/>-->
+<!--        </label>-->
+<!--        <label for="Bedrijfsnaam" class="form-label" >-->
+<!--            Bedrijfsnaam    <span class="star">*</span>-->
+<!--            <input id="Bedrijfsnaam"  type="text" required aria-required="true"/>-->
+<!--        </label>-->
+<!--        <label for="Bedrijfswebsite" class="form-label">-->
+<!--            Bedrijfswebsite  <span class="star">*</span>-->
+<!--            <input id="Bedrijfswebsite"    type="text" required aria-required="true" />-->
+<!--        </label>-->
 
 
-        <label for="ik-wil-graag-lid-worden-omdat" class="form-label " >
-            ik wil graag lid worden omdat    <span class="star">*</span>
-            <textarea id="ik-wil-graag-lid-worden-omdat" required aria-required="true">
+<!--        <label for="ik-wil-graag-lid-worden-omdat" class="form-label " >-->
+<!--            ik wil graag lid worden omdat    <span class="star">*</span>-->
+<!--            <textarea id="ik-wil-graag-lid-worden-omdat" required aria-required="true">-->
 
-            </textarea>
-        </label>
-        <label for="FTE">
-            FTE    <span class="star">*</span>
-            <select id="FTE" required aria-required="true" >
-                <option value="1-9">
-                    1-9
-                </option>
-                <option value="10-24">
-                    10-24
-                </option>
-                <option value="25-49">
-                    25-49
-                </option>
-                <option value="50-99">
-                    50-99
-                </option>
-                <option value="100-249">
-                    100-249
-                </option>
-                <option value="250+">
-                    250
-                </option>
-            </select>
-        </label>
-        <TextButton/>
-    </fieldset>
-</form>
+<!--            </textarea>-->
+<!--        </label>-->
+<!--        <label for="FTE">-->
+<!--            FTE    <span class="star">*</span>-->
+<!--            <select id="FTE" required aria-required="true" >-->
+<!--                <option value="1-9">-->
+<!--                    1-9-->
+<!--                </option>-->
+<!--                <option value="10-24">-->
+<!--                    10-24-->
+<!--                </option>-->
+<!--                <option value="25-49">-->
+<!--                    25-49-->
+<!--                </option>-->
+<!--                <option value="50-99">-->
+<!--                    50-99-->
+<!--                </option>-->
+<!--                <option value="100-249">-->
+<!--                    100-249-->
+<!--                </option>-->
+<!--                <option value="250+">-->
+<!--                    250-->
+<!--                </option>-->
+<!--            </select>-->
+<!--        </label>-->
+<!--        <TextButton/>-->
+<!--    </fieldset>-->
+<!--</form>-->
 
 <style>
     .star{
