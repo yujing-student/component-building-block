268918784190549f9687d36048398f219d54bd8d
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-11-14 12:02:39 +0100

    fix #7 I added the html code for the form element

---
+++ src/lib/Form.svelte
@@ -0,0 +1,77 @@
+
+<script>
+    import TextButton from '$lib/Subscribe.svelte'
+</script>
+
+<form>
+    <fieldset>
+        <legend>persoonlijke gegevens</legend>
+        <label for="Voornaam" class="form-label">
+            Voornaam*
+            <input id="Voornaam"  type="text" required
+
+            />
+        </label>
+        <label for="Tussenvoegsel" class="form-label">
+            Tussenvoegsel
+            <input id="Tussenvoegsel" type="text"/>
+        </label>
+        <label for="Achternaam" class="form-label">
+            Achternaam*
+            <input id="Achternaam"  type="text"/>
+        </label>
+        <label for="functie" class="form-label">
+            Functie*
+            <input id="functie" type="text"/>
+        </label>
+        <label for="email" class="form-label">
+            E-mail*
+            <input id="email" type="email"/>
+        </label>
+        <label for="Mobiel" class="form-label">
+            Mobiel*
+            <input id="Mobiel"  type="tel"/>
+        </label>
+        <label for="Bedrijfsnaam" class="form-label">
+            Bedrijfsnaam*
+            <input id="Bedrijfsnaam"  type="text"/>
+        </label>
+        <label for="Bedrijfswebsite" class="form-label">
+            Bedrijfswebsite*
+            <input id="Bedrijfswebsite"    type="text"/>
+        </label>
+        <label for="ik wil graag lid worden omdat" class="form-label ">
+            ik wil graag lid worden omdat*
+            <textarea>
+
+            </textarea>
+        </label>
+        <label for="FTE">
+            FTE*
+            <select id="FTE" >
+                <option value="">
+
+                </option>
+                <option value="1-9">
+                    1-9
+                </option>
+                <option value="10-24">
+                    10-24
+                </option>
+                <option value="25-49">
+                    25-49
+                </option>
+                <option value="50-99">
+                    50-99
+                </option>
+                <option value="100-249">
+                    100-249
+                </option>
+                <option value="250+">
+                    250
+                </option>
+            </select>
+        </label>
+        <TextButton/>
+    </fieldset>
+</form>
---
+++ src/lib/Subscribe.svelte
@@ -0,0 +1,24 @@
+<div>
+    <a href="bedankt" class="button">Schrijf me in</a>  <svg
+        width="16"
+        height="16"
+        viewBox="0 0 16 16"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+>
+    <path
+            d="M8 3L13.0001 8.0001L8 13.0002"
+            stroke="#222226"
+            stroke-width="1.5"
+            stroke-miterlimit="4.62023"
+    />
+    <path
+            d="M3 8L13 8"
+            stroke="#222226"
+            stroke-width="1.5"
+            stroke-miterlimit="4.62023"
+            stroke-linejoin="round"
+    />
+</svg>
+</div>
+
